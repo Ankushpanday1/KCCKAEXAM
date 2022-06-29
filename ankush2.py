bbc254ed9d402377d127ac3f230d3c4b0b85ff42
Ankush Panday <83767377+Ankushpanday1@users.noreply.github.com>
2022-06-29 13:05:23 +0530

    Create ankush2.py

---
+++ ankush2.py
@@ -0,0 +1,13 @@
+from math import factorial
+
+
+def C(n, k):
+    if n < k:
+        return 0
+    return factorial(n) // factorial(n-k)
+
+N = int(input())
+n_a = input().split().count('a')
+K = int(input())
+
+print('%.3f' % (1 - C(N - n_a, K) / C(N, K)))
